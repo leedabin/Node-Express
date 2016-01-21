84c9af6c447e10cf648e0f75cd3675e235dfb577
dabin <valve0807@gmail.com>
2016-01-21 22:40:13 +0900

    add homepage, about and Not Found

--- helloWorld.js
+++ helloWorld.js
@@ -1,8 +1,23 @@
 var http = require('http');
 
 http.createServer(function(req,res){
-   res.writeHead(200, { 'Content-Type': 'text/plain' });
-   res.end('Hello world!');
+   //url에서 쿼리스트링과 옵션인 마지막 슬래시를 지우고
+   //소문자로 바꿔서 정규화 합니다.
+   var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+   switch(path){
+      case '':
+   	res.writeHead(200, { 'Content-Type': 'text/plain' });
+   	res.end('Homepage');
+	break;
+      case '/about':
+   	res.writeHead(200, { 'Content-Type': 'text/plain' });
+   	res.end('About');
+	break;
+      default:
+   	res.writeHead(404, { 'Content-Type': 'text/plain' });
+   	res.end('Not Found');
+	break;
+   }
 }).listen(3000);
 
 console.log('Server started on localhost:3000; press Ctrl-C to terminate...');
